f9199285e5f3f894de0795cdfefea53f8cf6fbb5
odellanicolas-canch <odella.nicolas@gmail.com>
2026-01-16 10:54:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Landing cargada correctamente");
+document.getElementById("whatsappBtn").addEventListener("click", () => {
+  const phone = "5491158920330"; // tu WhatsApp
+  const text = encodeURIComponent("Hola, quiero más información");
+  window.location.href = `https://wa.me/${phone}?text=${text}`;
+});
